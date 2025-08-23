789ee1ac2b3485a4667814ab25f0a06d1863d50a
Gabriel Godoy <gabyreload@gmail.com>
2025-08-23 00:28:52 -0300

    validate textarea

--- src/controllers/note-create.php
+++ src/controllers/note-create.php
@@ -8,10 +8,18 @@ $heading = "Create Note";
 $currentUserId = 1;
 
 if($_SERVER["REQUEST_METHOD"] === "POST") {
-  $db->query('INSERT INTO notes (user_id, body) VALUES (:user_id, :body)', [
-    'user_id' => $currentUserId,
-    'body' => $_POST['body']
-  ]);
+    $errors = [];
+    if(strlen(trim($_POST['body'])) === 0) {
+        $errors['body'] = 'Body is required';
+    }
+
+
+    if(empty($errors)){
+        $db->query('INSERT INTO notes (user_id, body) VALUES (:user_id, :body)', [
+            'user_id' => $currentUserId,
+            'body' => $_POST['body']
+        ]);
+    }
 }
 
 require_once "views/note-create.view.php";
