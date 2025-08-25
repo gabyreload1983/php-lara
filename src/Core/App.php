c07ae1a6e72f831f29e0ca57e3f48176ba827fad
Gabriel Godoy <gabyreload@gmail.com>
2025-08-25 12:20:45 -0300

    App and Container

---
+++ src/Core/App.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core;
+
+class App {
+    protected static $container;
+
+    public static function setContainer($container) {
+        self::$container = $container;
+    }
+
+    public static function container() {
+        return self::$container;
+    }
+
+    public static function resolve($key) {
+        return self::$container->resolve($key);
+    }
+
+    public static function bind($key, $resolver) {
+        self::$container->bind($key, $resolver);
+    }   
+}
---
+++ src/Core/Container.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Core;
+
+class Container {
+    protected $bindings = [];
+
+    public function bind($key, $resolver){
+        $this->bindings[$key] = $resolver;
+    }
+
+    public function resolve($key){
+        if(isset($this->bindings[$key])){
+            return call_user_func($this->bindings[$key]);
+        }
+        throw new \Exception("No binding found for key {$key}");
+    }
+}   
