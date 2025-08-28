134abe92b337b2e490c6160072ab0055652b629d
Gabriel Godoy <gabyreload@gmail.com>
2025-08-27 22:28:51 -0300

    register user

---
+++ src/controllers/registration/store.php
@@ -0,0 +1,52 @@
+<?php
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+
+$email = $_POST['email'] ?? '';
+$password = $_POST['password'] ?? '';
+$errors = [];
+
+if(! Validator::email($email)) {
+    $errors['email'] = "A valid email is required.";
+}   
+
+if(! Validator::string($password, 8, 20)) {
+    $errors['password'] = "A password is required and must be between 8 and 20 characters.";
+}   
+
+if(! empty($errors)) {
+    return view("registration/create", [
+        'errors' => $errors
+    ]);
+}
+
+$db = App::resolve(Database::class);
+
+
+$user = $db->query('SELECT * FROM users WHERE email = :email', [
+    'email' => $email
+])->find();
+
+
+if($user) {
+    $errors['email'] = "Email is already registered.";
+    return view("registration/create", [
+        'errors' => $errors
+    ]);
+}
+
+if(!$user){
+    $db->query('INSERT INTO users (email, password) VALUES (:email, :password)', [
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_BCRYPT)
+    ]);
+
+    $_SESSION['user'] = [
+        'email' => $email
+    ];
+    
+    header('location: /');
+    exit();
+}
--- src/views/partials/nav.php
+++ src/views/partials/nav.php
@@ -38,10 +38,13 @@
                         <el-dropdown class="relative ml-3">
                             <button
                                 class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
+                                <?php if (isset($_SESSION['user'])): ?>
                                 <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                     alt="" class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                                <?php else: ?>
+                                <a href="/register"
+                                    class="cursor-pointer rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Register</a>
+                                <?php endif; ?>
                             </button>
 
                             <el-menu anchor="bottom end" popover
