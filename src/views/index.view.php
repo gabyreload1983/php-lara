f9370f926d6f8a53bdac0400bb6c4327f82b7e2f
Gabriel Godoy <gabyreload@gmail.com>
2025-08-30 14:33:03 -0300

    update message home page

--- src/views/index.view.php
+++ src/views/index.view.php
@@ -4,7 +4,7 @@
 
  <main>
      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-         <p class="text-white">Home Page</p>
+         <p class="text-white">Hello <?=  $_SESSION['user']['email'] ?? 'Guest' ?></p>
      </div>
  </main>
 
