2328b92b6e34aa7f8307c57a5700e71ae7391ca7
Gabriel Godoy <gabyreload@gmail.com>
2025-08-31 11:54:47 -0300

    refactor login

--- src/Http/controllers/session/store.php
+++ src/Http/controllers/session/store.php
@@ -1,7 +1,5 @@
 <?php
-use Core\App;
-use Core\Database;
-use Core\Validator;
+use Core\Authenticator;
 use Http\Forms\LoginForm;
 
 $email = $_POST['email'] ?? '';
@@ -9,35 +7,16 @@ $password = $_POST['password'] ?? '';
 
 $form = new LoginForm();
 
-if(! $form->validate($email, $password)){
-    return view("session/create", [
-        'errors' => $$form->errors()
-    ]);
+if($form->validate($email, $password)){
+    $auth = new Authenticator();
+
+    if($auth->attempt($email, $password)) {
+        redirect('/');
+    } 
+    
+    $form->error('email', "Invalid credentials.");
 }
 
-
-
-$db = App::resolve(Database::class);
-
-
-$user = $db->query('SELECT * FROM users WHERE email = :email', [
-    'email' => $email
-])->find();
-
-
-if($user) {
-    if(password_verify($password, $user['password'])){
-        
-        login([
-            'email'=> $email
-        ]);
-        
-        header('location: /');
-        exit();
-    }
-}
-
-$errors['email'] = "Invalid credentials.";
 return view("session/create", [
-    'errors' => $errors
+    'errors' => $form->errors()
 ]);
